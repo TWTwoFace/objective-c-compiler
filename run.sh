c505b55d1fa29ff10c15b4d0aa2988e7f67d3ce9
TWTwoFace <pipo2017@mail.ru>
2026-09-12 14:41:27 +0300

    fix: run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+set -e
 
 FLEX_BIN="./tools/flex.exe"
 GPP_BIN="/c/msys64/mingw64/bin/g++.exe"
+export PATH="/c/msys64/mingw64/bin:$PATH"
 
 "$FLEX_BIN" -o lexer.cpp lexer.l
 "$GPP_BIN" lexer.cpp -o my_scanner.exe
