22f8f860802c563bbc6570618e40da66e39f6ecd
TWTwoFace <pipo2017@mail.ru>
2026-09-13 11:48:37 +0300

    build: add errors.txt

--- run.sh
+++ run.sh
@@ -8,7 +8,8 @@ export PATH="/c/msys64/mingw64/bin:$PATH"
 "$FLEX_BIN" -o lexer.cpp lexer.l
 "$GPP_BIN" lexer.cpp -o my_scanner.exe
 
-./my_scanner.exe code.m > result.txt
+./my_scanner.exe code.m > result.txt 2> errors.txt
 
 echo "Done"
 start notepad result.txt
+start notepad errors.txt
